9c79dbb2dca5b039a490f5d9f593fc7cfc642324
whddus9509 <whddus9509@naver.com>
2020-07-05 19:43:37 +0900

    pratice Junit

---
+++ src/test/java/com/jongyeon/book/web/HelloControllerTest.java
@@ -0,0 +1,41 @@
+package com.jongyeon.book.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+;
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloReturnTest() throws Exception {
+        String hello= "Hello";
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDtoReturnTest() throws Exception {
+       String name="hello";
+       int amount = 1000;
+       mvc.perform(get("/hello/dto").param("name",name).param("amount",String.valueOf(amount)))
+               .andExpect(status().isOk());
+               //.andExpect((ResultMatcher) jsonPath("$.name",is(name)));
+               //.andExpect((ResultMatcher) jsonPath("$.amount",is(amount)));
+        //todo 에러수정할것
+
+    }
+}
