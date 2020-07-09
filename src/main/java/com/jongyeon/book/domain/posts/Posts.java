11a45645bc996ca6883ae7386eeee7f6b7a8f00a
whddus9509 <whddus9509@naver.com>
2020-07-09 22:41:18 +0900

    create jpa auditiing

--- src/main/java/com/jongyeon/book/domain/posts/Posts.java
+++ src/main/java/com/jongyeon/book/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jongyeon.book.domain.posts;
 
+import com.jongyeon.book.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Generated;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/jongyeon/book/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jongyeon/book/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +42,26 @@ public class PostsRepositoryTest {
             assertThat(posts.getTitle()).isEqualTo(title);
             assertThat(posts.getContent()).isEqualTo(content);
 
+        }
+
+        @Test
+        public void BaseTimeEntityTest(){
+            String title = "Test post";
+            String content= "test content";
+            LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+            postsRepository.save(Posts.builder()
+            .title(title)
+            .content(content)
+            .author("auothor")
+            .build());
+
+            List<Posts> postsList = postsRepository.findAll();
+
+            Posts posts = postsList.get(0);
+
+            System.out.println(">>>>>>>>>>> createDate ="+posts.getCreatedDate()+" ,modifiedDate="+posts.getModifiedDate());
+
+            assertThat(posts.getCreatedDate()).isAfter(now);
+            assertThat(posts.getModifiedDate()).isAfter(now);
         }
 }
