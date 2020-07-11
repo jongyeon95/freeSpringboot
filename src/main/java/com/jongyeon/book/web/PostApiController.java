5b36d43b31116f5727e8439086739c6e6d5633a5
whddus9509 <whddus9509@naver.com>
2020-07-11 16:17:03 +0900

    create deletPost

--- src/main/java/com/jongyeon/book/web/PostApiController.java
+++ src/main/java/com/jongyeon/book/web/PostApiController.java
@@ -28,6 +28,12 @@ public class PostApiController {
         return postsService.findById(id);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
 
 
 }
